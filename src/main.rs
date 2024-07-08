60573b17426c924857f8410edf4e10fb0e8dd3ee
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2024-07-08 13:02:53 +0000

    Attempt CLI and read GFF

---
+++ src/main.rs
@@ -0,0 +1,37 @@
+use std::{
+    fs::File,
+    io::{self, BufReader},
+};
+use clap::Parser;
+use noodles::gff as gff;
+
+// Note: Might achieve speed up with Tokio library.
+
+/// Validate a GFF3 file.
+#[derive(Parser)]
+struct Cli {
+    /// The path to the GGF3 file to read
+    path: std::path::PathBuf,
+}
+
+fn main() -> io::Result<()> {
+    let args = Cli::parse();
+    println!("GFF3 validation begun.");
+
+    let mut reader = File::open(args.path)
+        .map(BufReader::new)
+        .map(gff::io::Reader::new)?;
+
+    for result in reader.records() {
+        let record = result?;
+
+        println!(
+            "{}\t{}\t{}",
+            record.reference_sequence_name(),
+            record.start(),
+            record.end(),
+        );
+    }
+
+    Ok(())
+}
