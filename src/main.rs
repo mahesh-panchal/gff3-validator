10d820dbd5028409ac54c99139f19b23fe142189
Mahesh Binzer-Panchal <mahesh.binzer-panchal@nbis.se>
2024-09-13 07:21:58 +0000

    Reformat

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
+use clap::Parser;
+use jsonschema::{Draft, JSONSchema};
+use noodles::gff;
+use noodles::gff::record::{Phase, Strand};
+use noodles::gff::Record;
+use serde::Serialize;
+use serde_json::Value;
 use std::{
     fs,
     fs::File,
     io::{self, BufReader},
 };
-use clap::Parser;
-use noodles::gff as gff;
-use noodles::gff::Record;
-use noodles::gff::record::Strand;
-use noodles::gff::record::Phase;
-use serde::Serialize;
-use jsonschema::{Draft, JSONSchema};
-use serde_json::Value;
 
 // Note: Might achieve speed up with Tokio library.
 
 /// Validate a GFF3 file.
 #[derive(Parser)]
 struct Cli {
-    /// The path to the GGF3 file to read
+    // Path to GGF3 file to validate.
     gff3: std::path::PathBuf,
+    // JSON Schema against which to validate.
     json_schema: std::path::PathBuf,
 }
 
@@ -110,7 +110,7 @@ fn main() -> io::Result<()> {
         let my_record: MyGffRecord = record.into();
         let json = serde_json::to_string(&my_record).unwrap();
         let data_json: Value = serde_json::from_str(&json)?;
-        
+
         println!("{}", json);
         let validation_result = compiled_schema.validate(&data_json);
         if let Err(errors) = validation_result {
@@ -121,5 +121,6 @@ fn main() -> io::Result<()> {
         }
     }
 
+    println!("GFF3 validation complete.");
     Ok(())
 }
